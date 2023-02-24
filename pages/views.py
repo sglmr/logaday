2e4dee10213404cfd2489bf4ceadf7b7d912e84f
Stephen Gilmore <stephen@gilmore.us>
2023-02-24 11:19:26 -0800

    Set require methods on get views

--- pages/views.py
+++ pages/views.py
@@ -1,13 +1,17 @@
 from django.views.generic import TemplateView
+from django.views.decorators.http import require_http_methods
 
 
+@require_http_methods(["GET"])
 class HomePageView(TemplateView):
     template_name = "pages/home.html"
 
 
+@require_http_methods(["GET"])
 class AboutPageView(TemplateView):
     template_name = "pages/about.html"
 
 
+@require_http_methods(["GET"])
 class ProfilePageView(TemplateView):
     template_name = "pages/profile.html"
