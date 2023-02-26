3ae19b1039e7e16f8ff4d09fde44e123e87dd551
Stephen Gilmore <stephen@gilmore.us>
2023-02-25 21:45:41 -0800

    1 test in!

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+import pytest
+from django.conf import settings
+from accounts.models import CustomUser
 
-# Create your tests here.
+
+@pytest.fixture
+@pytest.mark.django_db
+def user1():
+    yield CustomUser.objects.create(
+        username="example", email="example@example.com", password="SuperSecure123!"
+    )
+
+
+@pytest.mark.django_db
+def test_new_user_fields(user1):
+    assert user1.username == "example"
+    assert user1.email == "example@example.com"
