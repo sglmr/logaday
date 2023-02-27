bbe0b3187d2c12cfb6eddc98c9339026503aee64
Stephen Gilmore <stephen@gilmore.us>
2023-02-26 16:21:01 -0800

    updated email settings

--- config/settings.py
+++ config/settings.py
@@ -148,16 +148,12 @@ if DEBUG:
     EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 else:
     EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-    DEFAULT_FROM_EMAIL = "stephen@logaday.net"
-    email = env.dj_email_url("EMAIL_URL", default="smtp://")
-    EMAIL_HOST = email["EMAIL_HOST"]
-    EMAIL_PORT = email["EMAIL_PORT"]
-    EMAIL_HOST_PASSWORD = email["EMAIL_HOST_PASSWORD"]
-    EMAIL_HOST_USER = email["EMAIL_HOST_USER"]
-    EMAIL_USE_TLS = email["EMAIL_USE_TLS"]
-
-# TODO: remove this after testing
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+    DEFAULT_FROM_EMAIL = env.str("DEFAULT_FROM_EMAIL")
+    EMAIL_HOST = env.str("EMAIL_HOST")
+    EMAIL_PORT = env.str("EMAIL_PORT")
+    EMAIL_HOST_PASSWORD = env.str("EMAIL_HOST_PASSWORD")
+    EMAIL_HOST_USER = env.str("EMAIL_HOST_USER")
+    EMAIL_USE_TLS = env.bool("EMAIL_USE_TLS")
 
 
 # https://docs.djangoproject.com/en/dev/topics/auth/customizing/#substituting-a-custom-user-model
