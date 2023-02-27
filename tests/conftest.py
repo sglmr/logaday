4fd42e597c5fc1dc92ffa1273644f67a7f49741c
Stephen Gilmore <stephen@gilmore.us>
2023-02-26 21:11:57 -0800

    test case data fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,7 @@
 import pytest
 from accounts.models import CustomUser
+from logapp.models import Log
+from django.utils import timezone
 
 
 @pytest.fixture
@@ -10,6 +12,24 @@ def user1():
 
 
 @pytest.fixture
-def user1_client(client, user1):
-    client.force_login(user1)
-    yield client
+def user2():
+    yield CustomUser.objects.create(
+        username="example2", email="example2@example.com", password="SuperSecure123!"
+    )
+
+
+@pytest.fixture
+def logs_fixt(user1, user2):
+    Log.objects.create(
+        user=user1,
+        date=timezone.now(),
+        title="user1_now_title",
+        content="user1_now_content",
+    )
+
+    Log.objects.create(
+        user=user2,
+        date=timezone.now(),
+        title="user2_now_title",
+        content="user2_now_content",
+    )
