d2225295781165ec61041aad45b1be9ae11fdd83
Stephen Gilmore <stephen@gilmore.us>
2023-02-27 21:19:21 -0800

    more test cases

--- tests/records/test_models.py
+++ tests/records/test_models.py
@@ -1,8 +1,8 @@
 import pytest
-from records.models import RecordSetting
+from records.models import RecordSetting, Record
 
 
-#      Records List Page Tests
+#    Record Settings Tests
 # -------------------------------
 @pytest.mark.django_db
 class TestRecordsSettingModel:
@@ -17,3 +17,18 @@ class TestRecordsSettingModel:
         assert user1.record_settings.headings == "personal, work"
         assert "# Personal" in user1.record_settings.default_content
         assert "# Work" in user1.record_settings.default_content
+
+    def test_record_settings__str__(self, user1):
+        assert f"{user1.record_settings}" == f"{user1} record settings"
+
+
+#         Record Tests
+# -------------------------------
+@pytest.mark.django_db
+class TestRecordsModel:
+    @pytest.fixture
+    def _record(self, user1, recs_fixt):
+        yield Record.objects.filter(user=user1).last()
+
+    def test_record__str__(self, _record):
+        assert f"{_record}" == _record.title
