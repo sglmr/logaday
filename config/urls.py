4a5479a80cdba7e4ef98d2908f80d7a72fe77470
Stephen Gilmore <stephen@gilmore.us>
2023-03-02 09:18:40 -0800

    changed admin url

--- config/urls.py
+++ config/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("knock/", admin.site.urls),
+    path("super/", admin.site.urls),
     path("", include("pages.urls")),
     path("accounts/", include("allauth.urls")),
     path("records/", include("records.urls")),
