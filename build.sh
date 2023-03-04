c5516b2d9c7c51443b441d3de6947bb9f92dd827
Stephen Gilmore <stephen@gilmore.us>
2023-03-04 10:37:17 -0800

    update build.sh

--- build.sh
+++ build.sh
@@ -6,13 +6,10 @@ cd /home/happy/recordmyday
 # Pull git changes
 git pull
 
-# Activate virtual environment
-source venv/bin/activate
-
 # pip commands
-pip install --upgrade pip | grep -v 'already satisfied'
-pip install -r requirements.txt | grep -v 'already satisfied'
-pip install -r requirements.txt --upgrade | grep -v 'already satisfied'
+python -m pip install --upgrade pip | grep -v 'already satisfied'
+python -m pip install -r requirements.txt | grep -v 'already satisfied'
+python -m pip install -r requirements.txt --upgrade | grep -v 'already satisfied'
 
 # Django commands
 python manage.py migrate
@@ -21,16 +18,13 @@ python manage.py collectstatic --noinput --clear | grep -v 'Deleting'
 
 # Refresh nginx & gunicorn
 echo "Restarting gunicorn & nginx..."
-sudo systemctl daemon-reload
 sudo systemctl restart recordmyday
-sudo systemctl restart nginx
+sudo systemctl restart caddy
+sudo systemctl daemon-reload
 echo "Done!"
 
 # Django deploy check
 python manage.py check --deploy
 
 # pip dependecy check
-pip list --outdated
-
-# Deactivate virtual environment
-deactivate
+python -m pip list --outdated
